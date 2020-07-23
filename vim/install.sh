e535439c6fa2df1d2e7929bdd75c77cb5c689ed0
suizman <suizman@suizman.org>
2020-07-23 23:45:50 +0200

    Update vim installer

--- vim/install.sh
+++ vim/install.sh
@@ -3,11 +3,11 @@
 if [ ! -d ~/.vim/colors ];
 then
   echo "Installing Plug."
-  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+  curl -sfLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-  vim -c :PlugInstall +qall!
-  echo "Updating Vim config."
-  mkdir ~/.vim/colors
+  cp vim/.vimrc ~/.vimrc
+  mkdir -p ~/.vim/colors
+  yes | vim +PlugInstall +qall &>/dev/null
   ln -s ~/.vim/plugged/gruvbox/colors/gruvbox.vim ~/.vim/colors/gruvbox.vim
 else
   echo "Config already updated."
