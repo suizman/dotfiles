5c74772958fd46b239e6fd343925f96d351a88ac
suizman <suizman@suizman.org>
2023-12-11 20:25:56 +0100

    Update fish installed

--- fish/install.sh
+++ fish/install.sh
@@ -6,11 +6,12 @@ then
   touch  ~/.config/fish/functions/_void.fish
   curl -L https://get.oh-my.fish | fish
   curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
-  fish -c 'fisher add danhper/fish-ssh-agent'
-  fish -c 'fisher add edc/bass'
-  fish -c 'fisher add jorgebucaran/fish-bax'
-  fish -c 'fisher add jorgebucaran/fish-spark'
-  fish -c 'fisher add jethrokuan/z'
+  omf install slavic-cat
+  fish -c 'fisher install danhper/fish-ssh-agent'
+  fish -c 'fisher install edc/bass'
+  fish -c 'fisher install jorgebucaran/fish-bax'
+  fish -c 'fisher install jorgebucaran/fish-spark'
+  fish -c 'fisher install jethrokuan/z'
 else
   echo "Fish goodies already installed."
 fi
