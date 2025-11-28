bbbe80ed3be3636dea2ae853ad4eadd003f12e0f
suizman <suizman@suizman.org>
2025-11-28 01:18:32 +0100

    Add fish config file

---
+++ fish/config/config.fish
@@ -0,0 +1,13 @@
+set -x suizman_dotfiles $HOME/work/repos/dotfiles
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    if type -q starship
+        starship init fish | source
+    end    
+
+    function fish_greeting
+        $suizman_dotfiles/scripts/printing/greeting.py --image $suizman_dotfiles/img/ascii/samurai
+    end
+end
+    
