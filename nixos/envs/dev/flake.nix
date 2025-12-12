f8622103a430e20ad7eacc6d8259d4ead508f74e
suizman <suizman@suizman.org>
2025-12-12 18:17:25 +0100

    Create Nix dev environment flake

---
+++ nixos/envs/dev/flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "Developement Flake for Nix";
+
+  # Use the latest stable Nixpkgs (as of Dec 2025)
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        # Development shell: run with `nix develop`
+        devShells.default = pkgs.mkShell {
+          name = "dev-nix";
+
+          # Packages available in the shell
+          buildInputs = with pkgs; [
+            # Programming
+            git
+            nodejs_24
+            python3
+            python3Packages.pip
+            python3Packages.virtualenv
+            go
+
+            # Web tools
+            wget
+
+            # Shell
+            fish
+            zsh
+            starship
+
+            # Terminals
+            kitty
+
+            # System
+            dig
+            glances
+            htop
+            tree
+            zip
+
+            # Editors
+            vim
+            nano
+          ];
+
+          # Configure fish as default shell
+          shellHook = ''
+            # Forzamos que nix develop use fish
+            exec ${pkgs.fish}/bin/fish
+          '';
+        };
+
+        # Optional: you can also expose packages, apps, etc.
+        packages.hello = pkgs.hello;
+
+        apps.hello = flake-utils.lib.mkApp { drv = pkgs.hello; };
+      });
+}
